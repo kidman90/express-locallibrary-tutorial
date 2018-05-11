0aa153c2cdd9f1f3266f4cbec217e907a511c3aa
kidman90 <kacprzak90@gmail.com>
2018-05-11 13:27:44 +0200

    Book list page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,7 +6,6 @@ var BookInstance = require('../models/bookinstance');
 var async = require('async');
 
 exports.index = function (req, res) {
-
   async.parallel({
     book_count: function (callback) {
       Book.count({}, callback); // Pass an empty object as match condition to find all documents of this collection
@@ -28,9 +27,15 @@ exports.index = function (req, res) {
   });
 };
 
-// Display list of all books.
-exports.book_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: Book list');
+// Display list of all Books.
+exports.book_list = function (req, res, next) {
+  Book.find({}, 'title author')
+    .populate('author')
+    .exec(function (err, list_books) {
+      if (err) { return next(err); }
+      //Successful, so render
+      res.render('book_list', { title: 'Book List', book_list: list_books });
+    });
 };
 
 // Display detail page for a specific book.
