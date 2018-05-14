085bc80350eedc66ec8e3a3533a5e9f0af2a2266
kidman90 <kacprzak90@gmail.com>
2018-05-14 16:33:30 +0200

    Author list page

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -23,6 +24,20 @@ AuthorSchema
   .virtual('url')
   .get(function () {
     return '/catalog/author/' + this._id;
+  });
+
+AuthorSchema
+  .virtual('lifespan')
+  .get(function () {
+    var lifetime_string = '';
+    if (this.date_of_birth) {
+      lifetime_string = moment(this.date_of_birth).format('MMMM Do, YYYY');
+    }
+    lifetime_string += ' - ';
+    if (this.date_of_death) {
+      lifetime_string += moment(this.date_of_death).format('MMMM Do, YYYY');
+    }
+    return lifetime_string
   });
 
 // Export model
